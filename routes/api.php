4e6798439a384e79fc93e710d160897ace2b062e
andres <echeverry11298@gmail.com>
2023-02-25 11:05:13 -0500

    api routes client  done

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/clients','App\Http\Controllers\ClientController@index');
+Route::post('/clients','App\Http\Controllers\ClientController@store');
+Route::get('/clients/{client}','App\Http\Controllers\ClientController@show');
+Route::put('/clients/{client}','App\Http\Controllers\ClientController@update');
+Route::delete('/clients/{client}','App\Http\Controllers\ClientController@destroy');
